f27076361f53e32eaafa4e459255afdc9dc4d62c
nurmanMV <nurman@mv-digital.com>
2024-05-01 07:18:55 +0700

    first commit

---
+++ lib/config.php
@@ -0,0 +1,17 @@
+<?php
+if(!defined('__NOT_DIRECT')){
+	//mencegah akses langsung ke file ini
+	die('Oops, akses tidak diizinkan!');
+}
+defined('__NOT_DIRECT') || define('__NOT_DIRECT',1);
+
+$host = "localhost"; //nama host
+$user = "root"; //nama user mysql
+$pass = ""; //password mysql
+$db = "db_umil"; //nama database 
+
+$upload_dir = "../../../upload";
+
+$connect = mysql_connect($host, $user, $pass);
+mysql_select_db($db, $connect) or die ("<b>Koneksi Database Gagal</b></br>".mysql_error());
+?>
